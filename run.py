b96b647075f921984bbf3e6772274ab7d0739d22
Johannes <johannesharz@gmail.com>
2026-08-19 21:03:59 +0200

    Add comment to indicate the start of the main execution block

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from app.versioning import format_version, load_version_data
 
 app = create_app(os.environ.get("FLASK_ENV", "development"))
 
+# Run
 if __name__ == "__main__":
     host = app.config.get("HOST", "0.0.0.0")
     port = app.config.get("PORT", 5000)
